245b6b88559d95ac4ae5e53d75da27db241d1ff9
MarceloMachadoxD <mastermanson@gmail.com>
2021-01-08 10:52:31 -0300

    Get /Products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsdeliver.repositories;
+
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
